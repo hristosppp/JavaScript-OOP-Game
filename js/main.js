d6dd66cf0817d11b13d2414eeeef8ce4b806e844
Hristos Pappasgit config --global user.name Hristos <hristos.pappas@icloud.com>
2023-03-02 19:01:50 +0800

    added functionality to move the player left/right

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-console.log("hello world")
+ class Player {
+    constructor(){
+        this.positionX = 0;
+        this.positionY = 0;
+        this.playerElm = document.getElementById("player");
+    }    
+
+    moveLeft(){
+        this.positionX--;   //if to manage not to go out the div(-)
+        this.playerElm.style.left = this.positionX + "vw";
+    }
+
+    moveRight(){
+        this.positionX++;
+        this.playerElm.style.left = this.positionX + "vw";
+    }
+}
+
+
+
+const myPlayer = new Player();
+
+
+document.addEventListener("keydown", (event) => {
+    if(event.code === "ArrowLeft"){
+         myPlayer.moveLeft();
+    }else if (event.key === "ArrowRight"){
+        myPlayer.moveRight();
+    }
+});
